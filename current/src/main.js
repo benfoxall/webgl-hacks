77c07dea006c7dd25eed4846f6cb569704e9bac5
Ben Foxall <benfoxall@gmail.com>
2016-12-27 15:03:22 +0000

    Refactoring

--- current/src/main.js
+++ current/src/main.js
@@ -1,40 +1,40 @@
-import {gl, createProgram, interleave, sendAttibutes} from './lib/gl.js'
+import { gl, createProgram, interleave, sendAttibutes } from './lib/gl.js'
+import { random } from './lib/util.js'
 
 import vertex from './vert.glsl'
 import fragment from './frag.glsl'
 
 const program = createProgram(vertex, fragment)
 
-var rows = 50, cols = 50;
+const rows = 50
+const cols = 50
+const n = rows * cols
 
-var n = rows * cols
-
-var data = new Float32Array(n * 7)
+const data = new Float32Array(n * 7)
 
 interleave(data, 7, (chunk, i) => {
-  var x = i % cols
-  var y = (i / cols)|0
+  const x = i % cols
+  const y = (i / cols) | 0
 
   chunk[0] = ((x/cols) - .5) * 1.5
   chunk[1] = ((y/rows) - .5) * 1.5
 
   // movement offset/phase
-  chunk[2] = Math.random() * Math.PI * 2
-  chunk[3] = Math.random() * Math.PI * 2
+  chunk[2] = random(0, Math.PI*2)
+  chunk[3] = random(0, Math.PI*2)
 
   // color
-  chunk[4] = Math.random()/2 + 0.5
-  chunk[5] = Math.random()/2 + 0.5
-  chunk[6] = Math.random()/2 + 0.5
+  chunk[4] = random(0, 1)
+  chunk[5] = random(0, 1)
+  chunk[6] = random(0, 1)
 
 })
 
 
+const indices = []
 
-var indices = []
-
-for (var x = 0; x < cols-1; x++) {
-  for (var y = 0; y < rows-1; y++) {
+for (let x = 0; x < cols - 1; x++) {
+  for (let y = 0; y < rows - 1; y++) {
 
     indices.push(
       x +     y * cols,
@@ -60,11 +60,12 @@ sendAttibutes(
 )
 
 
-var uTime = gl.getUniformLocation(program, 'uTime')
+const uTime = gl.getUniformLocation(program, 'uTime')
 
 function render(t) {
   requestAnimationFrame(render)
-  gl.uniform1f(uTime, t/10000)
+
+  gl.uniform1f(uTime, t / 10000)
   gl.drawElements(gl.LINES, indices.length, gl.UNSIGNED_SHORT, 0)
 }
 
