ca7578f9d4c4c71678fe32cdeef6da83c04f8cef
Ben Foxall <benfoxall@gmail.com>
2017-01-03 22:00:01 +0000

    big circle thing

--- current/src/main.js
+++ current/src/main.js
@@ -2,6 +2,7 @@ import { gl, createProgram, interleave, sendAttibutes } from './lib/gl.js'
 import { random, flatten } from './lib/util.js'
 
 import icosphere from 'icosphere'
+import mat4 from 'gl-mat4'
 
 import vertex from './vert.glsl'
 import fragment from './frag.glsl'
@@ -10,7 +11,7 @@ gl.enable(gl.DEPTH_TEST)
 
 const program = createProgram(vertex, fragment)
 
-const mesh = icosphere(2)
+const mesh = icosphere(4)
 
 const n = mesh.positions.length
 
@@ -47,10 +48,24 @@ sendAttibutes(
 )
 
 const uTime = gl.getUniformLocation(program, 'uTime')
+const uTransform = gl.getUniformLocation(program, 'uTransform')
+
+const transform = mat4.create()
+const I = mat4.create()
 
 function render(t) {
   requestAnimationFrame(render)
   gl.uniform1f(uTime, t / 10000)
+
+  mat4.rotateY(transform, I, t/3000)
+
+  mat4.rotateX(transform, transform, Math.sin(t/1000)/3)
+
+
+  // mat4.rotateX(transform, transform, Math.sin(t/200)/30)
+
+  gl.uniformMatrix4fv(uTransform, false, transform)
+
   gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0)
 }
 
--- current/src/vert.glsl
+++ current/src/vert.glsl
@@ -2,27 +2,49 @@ attribute vec3 aPosition;
 attribute vec2 aPhase;
 attribute vec3 aColor;
 uniform float uTime;
+uniform mat4 uTransform;
 
 varying vec3 vColor;
 
 void main () {
 
-  float t = uTime * 20.0;
+  float t = uTime * 5.0;
+  //
+  // vec3 off = vec3(
+  //   sin(t + aPhase.x),
+  //   sin(t + aPhase.y),
+  //   0.0
+  // ) * 0.02;
+  //
+  // vec3 off2 = vec3(
+  //   sin(t + aPosition.x + aPosition.y),
+  //   cos(t + aPosition.y + aPosition.x*4.0),
+  //   cos(t + aPosition.x + aPosition.z*4.0)
+  // ) * 0.04;
+
 
   vec3 off = vec3(
     sin(t + aPhase.x),
     sin(t + aPhase.y),
     0.0
-  ) * 0.02;
+  ) * 0.001;
 
   vec3 off2 = vec3(
     sin(t + aPosition.x*2.0 + aPosition.y*3.0),
     sin(t + aPosition.y + aPosition.x*4.0),
     0.0
-  ) * 0.04;
+  ) * 0.05;
 
-  gl_Position = vec4(aPosition+off+off2, 1.0);
+  vec3 pos = aPosition+off+off2;
 
-  vColor = aColor;
+  gl_Position = uTransform * vec4(pos, 1.0);
+
+  vColor = dot(pos, vec3(0.0,1.0,0.0)) * vec3(1.0,1.0,1.0);
+
+  vColor.r *= pos.x;
+  vColor.g *= pos.z;
+
+
+  gl_PointSize = 30.0;
 
 }
